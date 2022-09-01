f58f1353a6e23159855807fe238c1f0e55491cdb
ramblehead <v.rybynok@gmail.com>
2022-09-01 21:55:06 +0100

    Checkpoint

--- packages/binary-gap-js/src/main.js
+++ packages/binary-gap-js/src/main.js
@@ -1,9 +1,40 @@
 // Hey Emacs, this is -*- coding: utf-8 -*-
 
+/* eslint-disable no-bitwise */
+/* eslint-disable no-plusplus */
+
 /**
  * @param {number}  N
  * @returns {number}
  */
 export const solution = (N) => {
-  return 42;
+  if (!(Number.isInteger(N) && N >= 1 && N <= 2_147_483_647)) {
+    throw Error(`N = ${N} is outside valid range`);
+  }
+
+  const binaryWidth = 31;
+
+  let binaryGap = 0;
+  let currentGap = 0;
+  let gapTrack = false;
+  let mask = 1;
+
+  for (let i = 0; i < binaryWidth; ++i) {
+    if ((N & mask) !== 0) {
+      if (gapTrack) {
+        if (currentGap > binaryGap) {
+          binaryGap = currentGap;
+        }
+        currentGap = 0;
+      } else {
+        gapTrack = true;
+      }
+    } else if (gapTrack) {
+      ++currentGap;
+    }
+
+    mask <<= 1;
+  }
+
+  return binaryGap;
 };
--- packages/binary-gap-js/src/start.js
+++ packages/binary-gap-js/src/start.js
@@ -6,5 +6,11 @@ import { solution } from './main.js';
  * @returns {Promise<void>}
  */
 (async () => {
-  console.log(solution(10));
+  console.log('Binary gap samples:');
+  console.log(Number(10).toString(2), '->', solution(10));
+  console.log(
+    Number(0b100_0000_0000_0000_0000_0000_0000_1010).toString(2),
+    '->',
+    solution(0b100_0000_0000_0000_0000_0000_0000_1010),
+  );
 })();
