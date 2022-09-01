015430fb6fe664abf43dad56ff54020f2010b7f0
ramblehead <v.rybynok@gmail.com>
2022-09-01 21:59:58 +0100

    Checkpoint

--- packages/binary-gap-js/src/main.test.js
+++ packages/binary-gap-js/src/main.test.js
@@ -5,10 +5,24 @@ import assert from 'assert';
 
 import { solution } from './main.js';
 
-describe('Array', () => {
-  describe('BinaryGap solution()', () => {
-    it('should return 42', () => {
-      assert.equal(solution(10), 42);
-    });
+describe('BinaryGap solution()', () => {
+  it('should return 2', () => {
+    assert.equal(solution(9), 2);
+  });
+
+  it('should return 4', () => {
+    assert.equal(solution(529), 4);
+  });
+
+  it('should return 1', () => {
+    assert.equal(solution(20), 1);
+  });
+
+  it('should return 0', () => {
+    assert.equal(solution(15), 0);
+  });
+
+  it('should return 0', () => {
+    assert.equal(solution(32), 0);
   });
 });
