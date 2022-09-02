43de072072632a362aed4693a0be9a4067526764
ramblehead <v.rybynok@gmail.com>
2022-09-02 19:53:52 +0100

    Checkpoint

--- packages/binary-gap/nodejs/src/perf.js
+++ packages/binary-gap/nodejs/src/perf.js
@@ -2,9 +2,29 @@
 
 import { solution } from './main.js';
 
+const CYCLES_COUNT = 1_000_000;
+
 /**
  * @returns {Promise<void>}
  */
 (async () => {
-  console.log('Binary gap solution() exec duration:');
+  const nums = Array.from({ length: CYCLES_COUNT }, (_v, _i) => {
+    return Math.ceil(Math.random() * 2_147_483_647);
+  });
+
+  let binaryGap;
+  let durationAvg = 0;
+
+  nums.forEach((N) => {
+    const start = performance.now();
+    binaryGap = solution(N);
+    const duration = performance.now() - start;
+    durationAvg += duration;
+  });
+
+  durationAvg /= CYCLES_COUNT;
+
+  console.log(
+    `Binary gap solution() exec duration: ${durationAvg * 10 ** 3} ns`,
+  );
 })();
