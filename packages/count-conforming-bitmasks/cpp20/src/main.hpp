e9ebcdd69a8c445c99bf93686e40a20abe7607b2
ramblehead <v.rybynok@gmail.com>
2022-09-08 12:21:56 +0100

    Seems to be working (once again)

--- packages/count-conforming-bitmasks/cpp20/src/main.hpp
+++ packages/count-conforming-bitmasks/cpp20/src/main.hpp
@@ -16,9 +16,9 @@ inline auto solution(int32_t A, int32_t B, int32_t C) -> int32_t {
   assert(A >= 0 && C <= 1'073'741'823);
   constexpr int binaryWidth = 30;
 
-  const int_fast32_t ua = A;
-  const int_fast32_t ub = B;
-  const int_fast32_t uc = C;
+  const int_fast32_t a = A;
+  const int_fast32_t b = B;
+  const int_fast32_t c = C;
 
   int az = 0;
   int bz = 0;
@@ -33,51 +33,50 @@ inline auto solution(int32_t A, int32_t B, int32_t C) -> int32_t {
   int_fast32_t mask = 1;
 
   for (int i = 0; i < binaryWidth; ++i) {
-    if (((ua & mask) == 0) && ((ub & mask) == 0) && ((uc & mask) == 0)) {
+    if (((a & mask) == 0) && ((b & mask) == 0) && ((c & mask) == 0)) {
       abcz += 1;
     }
-    else {
-      if (((ua & mask) == 0) && ((ub & mask) == 0)) {
-        abz += 1;
-      }
-      else if (((ub & mask) == 0) && ((uc & mask) == 0)) {
-        bcz += 1;
-      }
-      else if (((ua & mask) == 0) && ((uc & mask) == 0)) {
-        acz += 1;
-      }
+
+    if (((a & mask) == 0) && ((b & mask) == 0)) {
+      abz += 1;
+    }
+    if (((b & mask) == 0) && ((c & mask) == 0)) {
+      bcz += 1;
+    }
+    if (((a & mask) == 0) && ((c & mask) == 0)) {
+      acz += 1;
     }
 
-    if ((ua & mask) == 0) {
+    if ((a & mask) == 0) {
       az += 1;
     }
-    if ((ub & mask) == 0) {
+    if ((b & mask) == 0) {
       bz += 1;
     }
-    if ((uc & mask) == 0) {
+    if ((c & mask) == 0) {
       cz += 1;
     }
 
     mask <<= 1;
   }
 
-  const int_fast32_t powAz = (1 << az) - 1;
-  const int_fast32_t powBz = (1 << bz) - 1;
-  const int_fast32_t powCz = (1 << cz) - 1;
+  const int_fast32_t powAz = (1 << az);
+  const int_fast32_t powBz = (1 << bz);
+  const int_fast32_t powCz = (1 << cz);
 
-  const int_fast32_t powAbz = (1 << abz) - 1;
-  const int_fast32_t powBcz = (1 << bcz) - 1;
-  const int_fast32_t powAcz = (1 << acz) - 1;
+  const int_fast32_t powAbz = (1 << abz);
+  const int_fast32_t powBcz = (1 << bcz);
+  const int_fast32_t powAcz = (1 << acz);
 
-  const int_fast32_t powAbcz = (1 << abcz) - 1;
+  const int_fast32_t powAbcz = (1 << abcz);
 
-  std::cout << "az " << +az << "\n";
-  std::cout << "bz " << +bz << "\n";
-  std::cout << "cz " << +cz << "\n";
-  std::cout << "abz " << +abz << "\n";
-  std::cout << "bcz " << +bcz << "\n";
-  std::cout << "acz " << +acz << "\n";
-  std::cout << "abcz " << +abcz << "\n";
+  std::cout << "az " << az << "\n";
+  std::cout << "bz " << bz << "\n";
+  std::cout << "cz " << cz << "\n";
+  std::cout << "abz " << abz << "\n";
+  std::cout << "bcz " << bcz << "\n";
+  std::cout << "acz " << acz << "\n";
+  std::cout << "abcz " << abcz << "\n";
 
   std::cout << "~~~~~~~~\n";
 
@@ -89,17 +88,15 @@ inline auto solution(int32_t A, int32_t B, int32_t C) -> int32_t {
   std::cout << "powAcz " << powAcz << "\n";
   std::cout << "powAbcz " << powAbcz << "\n";
 
-  // const int_fast32_t powAbcz = 1 << (abcz + 1);
-  // const int_fast32_t powAbcz = ((1 << abcz) - 1) * 4;
+  const int_fast32_t abUnique = powAbz - powAbcz;
+  const int_fast32_t bcUnique = powBcz - powAbcz;
+  const int_fast32_t acUnique = powAcz - powAbcz;
 
-  // return powAz + powBz + powCz - powAbz - powBcz - powAcz - powAbcz - 2;
+  const int_fast32_t aUnique = powAz - abUnique - acUnique - powAbcz;
+  const int_fast32_t bUnique = powBz - abUnique - bcUnique - powAbcz;
+  const int_fast32_t cUnique = powCz - bcUnique - acUnique - powAbcz;
 
-  const int_fast32_t aConf = powAz;
-  const int_fast32_t bConf = powBz - powAbz - powAbcz * (abz + bcz + acz);
-  const int_fast32_t cConf =
-      powCz - powAcz - powBcz - powAbcz * (abz + bcz + acz);
-
-  return aConf + bConf + cConf + 1;
+  return aUnique + bUnique + cUnique + abUnique + bcUnique + acUnique + powAbcz;
 }
 
 #endif // __count_conforming_bitmasks_hpp__
