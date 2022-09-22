ef09b18c3fc1c16e653b228710b0f485b67c7f91
ramblehead <v.rybynok@gmail.com>
2022-09-23 00:51:25 +0100

    Checkpoint

--- packages/test0/cpp14/src/main.hpp
+++ packages/test0/cpp14/src/main.hpp
@@ -14,11 +14,37 @@
 // #include "cpp-utils/binary_form.hpp"
 
 // NOLINTNEXTLINE(modernize-use-trailing-return-type)
+inline bool isSparse(std::uint32_t n) {
+  constexpr int width = std::numeric_limits<std::uint32_t>::digits;
+
+  std::uint32_t mask = 1;
+
+  for (int i = 0; i < width; ++i) {
+    const auto constituent = n & mask;
+
+    if (constituent != 0) {
+      if (i > 0) {
+        const auto prevMask = mask >> 1U;
+        const auto prevConstituent = n & prevMask;
+
+        if (prevConstituent != 0) {
+          return false;
+        }
+      }
+    }
+
+    mask <<= 1U;
+  }
+
+  return true;
+}
+
+// NOLINTNEXTLINE(modernize-use-trailing-return-type)
 inline std::int32_t solution(std::int32_t N) {
   // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
   assert(N >= 0 && N <= 1'000'000'000);
 
-  const int width = std::numeric_limits<std::uint32_t>::digits;
+  constexpr int width = std::numeric_limits<std::uint32_t>::digits;
 
   const std::uint32_t n = N;
   std::uint32_t mask = 1;
@@ -26,13 +52,25 @@ inline std::int32_t solution(std::int32_t N) {
 
   for (int i = 0; i < width; ++i) {
     const auto constituent = n & mask;
+
     if (constituent != 0) {
       constituents.push_back(constituent);
+
+      if (i > 0) {
+        const auto prevMask = mask >> 1U;
+        const auto prevConstituent = n & prevMask;
+
+        if (prevConstituent == 0) {
+          constituents.push_back(prevMask);
+        }
+      }
     }
+
     mask <<= 1U;
   }
 
   std::cout << ranges::views::all(constituents) << std::endl;
+  std::cout << isSparse(0b1001010) << std::endl;
 
   return N;
 }
